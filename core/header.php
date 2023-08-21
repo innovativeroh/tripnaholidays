8263734b9129e3db037bc34b3eade12bf3ec3673
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-08-21 15:53:33 +0530

    Project Setup Has Been Completed

---
+++ core/header.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="./resource/css/main.css">
+<div id="wrapper">
+    <header>
+        <div class='flexContainer'>
+            <div id="logoArea">
+                <img src='./resource/img/logo.png'>
+            </div>
+            <nav>
+                <a href='#'>Why Us</a>
+                <a href='#'>About Us</a>
+                <button id='mainBtn'>Login</button>
+                <button id='whatsappBtn'>Whatsapp</button>
+            </nav>
+        </div>
+    </header>
+</div>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <?php include_once("./core/header.php");?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get your tourist visa, quick and easy | Tripnaholidays</title>
+</head>
+<body>
+
+</body>
+</html>
