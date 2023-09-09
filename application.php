3d5ab2da69e62894b10749774326c9ef95e81a9c
Rohan Sidhu <rohan.sidhu@outlook.com>
2023-09-09 16:17:04 +0530

    Frontend Has Been Completed

--- application.php
+++ application.php
@@ -1,20 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    <?php include_once("./core/header.php");?>
+
+<head>
+    <?php include_once("./core/header.php"); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Get your tourist visa, quick and easy | Tripnaholidays</title>
 </head>
+
 <body style='background: #f1f1f1;'>
     <div class='halfBack'>
         <br>
-    <div class='defaultApplication' style='max-width: 1200px; margin-right: auto;
-    margin-left: auto; margin-top: 120px;'>
+        <div class='defaultApplication' style='max-width: 1200px; margin-right: auto;
+    margin-left: auto; margin-top: 120px; box-shadow: 1px 1px 10px #ccc;'>
             <div style='padding: 20px;'>
                 <p style='font-size: 18px; color: #555; font-weight: 600; display: inline;'>Traveller</p>
             </div>
             <hr>
+            <?php
+            if (isset($_POST['verifyPassport'])) {
+                $passNumber = @$_POST['passNumber'];
+                $name = @$_POST['name'];
+                $sur_name = @$_POST['surname'];
+                $gender = @$_POST['gender'];
+                $dob = @$_POST['dob'];
+                $pobPassport = @$_POST['pobPassport'];
+                $exPassport = @$_POST['exPassport'];
+                $address1 = @$_POST['address1'];
+                $address2 = @$_POST['address2'];
+                $city = @$_POST['city'];
+                $state = @$_POST['state'];
+                $pincode = @$_POST['pincode'];
+                $mobile = @$_POST['mobile'];
+                $email = @$_POST['email'];
+            }
+            ?>
             <div style='padding: 20px;'>
                 <div class='flexFormContainer'>
                     <div id='flexForm' style='flex: 1.4;'>
@@ -24,73 +44,77 @@
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Passport number</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Passport Number'>
+                                    <input type='input' name='passNumber' class='input_size'
+                                        placeholder='Passport Number'>
                                 </div>
                             </div>
                             <br>
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Given name (as on Passport)</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Given name'>
+                                    <input type='input' name='name' class='input_size' placeholder='Given name'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Surname (as on Passport)</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Surname'>
+                                    <input type='input' name='surname' class='input_size' placeholder='Surname'>
                                 </div>
                             </div>
                             <br>
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Sex</label><br>
-                                    <select class='input_size'>
+                                    <select name='gender' class='input_size'>
                                         <option value='Male'>Male</option>
                                         <option value='Female'>Female</option>
                                         <option value='Others'>Others</option>
                                     </select>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
-                                    <label>Surname (as on Passport)</label><br>
-                                    <input type='date' name='passNumber' class='input_size'>
+                                    <label>Date Of Birth</label><br>
+                                    <input type='date' name='dob' class='input_size'>
                                 </div>
                             </div>
                             <br>
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Place of birth</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Place of birth'>
+                                    <input type='input' name='pobPassport' class='input_size'
+                                        placeholder='Place of birth'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Passport expiry date</label><br>
-                                    <input type='date' name='passNumber' class='input_size'>
+                                    <input type='date' name='exPassport' class='input_size'>
                                 </div>
                             </div>
                             <br><br>
                             <p style='font-weight: 800; color: var(--color-dark-gray);'>Review 's basic details:</p><br>
-                            <input type='checkbox' name='resident'> <p style='display: inline;'>My address in passport is same as current residential address</p>
+                            <input type='checkbox' name='resident'>
+                            <p style='display: inline;'>My address in passport is same as current residential address
+                            </p>
                             <br><br>
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Address 1</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Address 1'>
+                                    <input type='input' name='address1' class='input_size' placeholder='Address 1'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
-                                <label>Address 2</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Address 2'>
+                                    <label>Address 2</label><br>
+                                    <input type='input' name='address2' class='input_size' placeholder='Address 2'>
                                 </div>
                             </div>
                             <br>
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>City</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='City'>
+                                    <input type='input' name='city' class='input_size' placeholder='City'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Address 1</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Select State'>
+                                    <input type='input' name='state' class='input_size' placeholder='Select State'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
-                                <label>Address 2</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Enter Pincode'>
+                                    <label>Address 2</label><br>
+                                    <input type='input' name='pincode' class='input_size' placeholder='Enter Pincode'>
                                 </div>
                             </div>
                             <br>
@@ -99,14 +123,15 @@
                             <div class='formFlex'>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Phone</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='City'>
+                                    <input type='tel' name='mobile' class='input_size' placeholder='Mobile'>
                                 </div>
                                 <div id='flexForm' style='flex: 1;'>
                                     <label>Email</label><br>
-                                    <input type='input' name='passNumber' class='input_size' placeholder='Select State'>
+                                    <input type='email' name='email' class='input_size' placeholder='Email'>
                                 </div>
                             </div><br>
-                            <button type="submit" class="input_btn">Veify Traveller Details</button>
+                            <button type="submit" name='verifyPassport' class="input_btn">Veify Traveller
+                                Details</button>
                         </form>
                     </div>
                     <div id='flexForm' style='flex: 1;'>
@@ -119,5 +144,7 @@
                     </div>
                 </div>
             </div>
-            <?php include_once("./core/footer.php");?>
+        </div>
+        <br><br>
+        <?php include_once("./core/footer.php"); ?>
 </body>
---
+++ payment.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once("./core/header.php"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get your tourist visa, quick and easy | Tripnaholidays</title>
+</head>
+
+<body style='background: #f1f1f1;'>
+    <div class='halfBack'>
+        <br>
+        <div id='wrapper' style='margin-top: 180px;'>
+            <div class='paymentFlexContainer'>
+                <div id='flexBox' style='flex: 1;'>
+                    <div style='margin-top: 100px; padding: 20px;'>
+                        <button><i class="fa-solid fa-arrow-left-long"></i></button><br>
+                        <h1 style='font-weight: 600;'>Complete your<br>payment</h1>
+                        <span>Payment summary:</span>
+                        <br><br>
+                        <form action='#' method='POST' style='display: flex; gap: 10px;'>
+                            <input type='text' name='code' placeholder='Apply coupon code' style='flex: 1;'
+                                class='couponCodeInput'>
+                            <button type='submit'>Apply</button>
+                        </form><br>
+                        <hr style='height: 1px; border: 0 none; background: #ccc;'>
+                        <br>
+                        <div class='calcFlexContainer'>
+                            <div id='flexBox'>
+                                <p style='font-weight: 600; font-size: 18px; line-height: 30px;'>Embassy & Appointment
+                                    fees</p>
+                                <p>₹ 9,800 for ALPHIUS<br>SUCHARITHA STANY SAHAYARAJ</p>
+                            </div>
+                            <div id='flexBox'>
+                                <p style='text-align: right; font-size: 18px; font-weight: 500;'>₹9,800</p>
+                            </div>
+                        </div>
+                        <br>
+                        <hr style='height: 1px; border: 0 none; background: #ccc;'>
+                        <br>
+                        <div class='calcFlexContainer'>
+                            <div id='flexBox'>
+                                <p style='font-weight: 600; font-size: 18px; line-height: 30px;'>Teleport fee</p>
+                                <p>₹599(per traveller) x 1</p>
+                            </div>
+                            <div id='flexBox'>
+                                <p style='text-align: right; font-size: 18px; font-weight: 500;'>₹599</p>
+                            </div>
+                        </div>
+                        <br>
+                        <hr style='height: 1px; border: 0 none; background: #ccc;'>
+                        <br>
+                        <div class='calcFlexContainer'>
+                            <div id='flexBox'>
+                                <p style='font-weight: 600; font-size: 18px; line-height: 30px;'>Grand total</p>
+                            </div>
+                            <div id='flexBox'>
+                                <p style='text-align: right; font-size: 18px; font-weight: 500;'>₹10,399</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='flexBox' style='flex: 1;'>
+                    <div class='cardStack'>
+                        <center>
+                            <form action='#' method="POST" >
+                            <h2 style='font-weight: 600;'>Pay only Teleport<br>fees now<h2>
+                                    <br><h1 style='font-size: 36px;'>₹599</h1><br>
+                                    <hr style='background: #eee; height: 1px; border: 0 none;'>
+                                </center>
+                                    <br><br>
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Pay embassy fees later</p>
+<br>
+                                    
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Fully refundable, if you change your mind.</p>
+<br>
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Dedicated chat support.</p>
+                                    <br><br>
+                                    <button type='Submit'>Pay Now!</button>
+</form>
+                    </div>
+                </div>
+                <div id='flexBox' style='flex: 1;'>
+                <div class='cardStack'>
+                        <center>
+                            <form action='#' method="POST" >
+                            <h2 style='font-weight: 600;'>Pay entire fees<br>now<h2>
+                                    <br><h1 style='font-size: 36px;'>₹10,399</h1><br>
+                                    <hr style='background: #eee; height: 1px; border: 0 none;'>
+                                </center>
+                                    <br><br>
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Pay nothing later</p>
+<br>
+                                    
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Fully refundable, before visa application submission</p>
+<br>
+                                    <p style='font-size: 18px;'><i class="fa-solid fa-check" style='padding-right: 10px; color: #279EFF;'></i> Dedicated relationship manager</p>
+                                    <br><br>
+                                    <button type='Submit'>Pay Now!</button>
+</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include_once("./core/footer.php"); ?>
+</body>
+<html>
